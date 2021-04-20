162af404d4a077bb6715e96d5b2ec79e3449eff8
Your Name <you@example.com>
2021-04-21 07:59:15 +0900

    first commit

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo apt install texlive-base \\
- texlive-latex-recommended \\
- texlive \\
- texlive-latex-extra \\
- texlive-full \\
- texlive-publishers \\
- texlive-science \\
- texlive-pstricks \\
- texlive-pictures \\
+sudo apt install texlive-base \
+ texlive-latex-recommended \
+ texlive \
+ texlive-latex-extra \
+ texlive-full \
+ texlive-publishers \
+ texlive-science \
+ texlive-pstricks \
+ texlive-pictures \
  texlive-lang-japanese 
